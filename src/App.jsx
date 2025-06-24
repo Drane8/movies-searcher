c3974677fd9ee77bbabecb228dac3bae2b83f871
Daniel Garcia <danigarciaberzal@gmail.com>
2025-06-24 19:05:02 +0200

    feat: add like button to moviecard

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import { MovieService } from "./core/service/MovieService";
 import { JSONMovieRepository } from "./core/infraestructure/JSONMovieRepository";
 
 const Movie = ({ title, year }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const handleLikeClick = () => {
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="movie-card">
       <div className="movie-info">
         <h3 className="movie-title">{title}</h3>
         <p className="movie-year">{year}</p>
       </div>
+      <button className="like-button" onClick={handleLikeClick} type="button">
+        {isLiked ? "❤️" : "🤍"} Me gusta
+      </button>
     </div>
   );
 };
